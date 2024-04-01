fda917437bc9d0f5167ebd2dcc6aeb5fdfc86539
unknown <varsha.hubresolution@gmail.com>
2024-04-01 10:17:13 +0530

    update on https

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 const fs = require('fs')
 const https = require('https')
+const http = require('http')
 
 
 const userRoute = require('./route/api/userRoute');
@@ -34,18 +35,29 @@ app.use('/' , sizeRoute)
 const  colorRoute = require('./route/api/colorRoute')
 app.use('/' , colorRoute)
 
+app.get( '/test' , (req , res)=>{
+    res.send("hey")
 
+})
 
 const options = {
     key: fs.readFileSync("./key/private.key"), // replace it with your key path
     cert: fs.readFileSync("./key/certificate.crt"), // replace it with your certificate path
 }
 
-https.createServer(options, (req, res) => {
-    res.writeHead(200);
-    res.end('Hello, HTTPS World!');
-  }).listen(process.env.PORT, '0.0.0.0', () => {
-    console.log('Server is running on port 8080');
-  });
+// https.createServer(options, (req, res) => {
+//     res.writeHead(200);
+//     res.end('Hello, HTTPS World!');
+//   }).listen(443 , () => {
+//     console.log('Server is running on port 8080');
+//   });
 
-// app.listen(process.env.PORT, '0.0.0.0')
+app.get('/', (req, res) => {
+    res.send('Now using https..');
+ });
+ 
+ var server = https.createServer(options, app);
+ 
+ server.listen(443, () => {
+   console.log("server starting on port : " + 443)
+ });
